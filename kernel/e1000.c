204e02006f9b2fc81ec110e5f90a07ab16c6de2f
harry <harry4531849@gmail.com>
2024-01-07 14:53:56 +0800

    finish net

--- kernel/e1000.c
+++ kernel/e1000.c
@@ -95,14 +95,36 @@ e1000_init(uint32 *xregs)
 int
 e1000_transmit(struct mbuf *m)
 {
-  //
+  //https://zhuanlan.zhihu.com/p/556256038
+  //https://zhuanlan.zhihu.com/p/593943193
+  //https://zhuanlan.zhihu.com/p/610448499
+  //https://juejin.cn/post/7227013497040404539
+  //https://blog.csdn.net/LostUnravel/article/details/121437373
+  //https://zhuanlan.zhihu.com/p/351563871
+  //https://zhuanlan.zhihu.com/p/351563871
   // Your code here.
   //
   // the mbuf contains an ethernet frame; program it into
   // the TX descriptor ring so that the e1000 sends it. Stash
   // a pointer so that it can be freed after sending.
   //
-  
+  acquire(&e1000_lock);
+  int tx_transmit_index = regs[E1000_TDT];
+  if((tx_ring[tx_transmit_index].status & E1000_TXD_STAT_DD) == 0){
+    release(&e1000_lock);
+    return -1;
+  }
+  if(tx_mbufs[tx_transmit_index]){
+    mbuffree(tx_mbufs[tx_transmit_index]);
+  }
+  tx_ring[tx_transmit_index].addr = (uint64) m->head;
+  tx_ring[tx_transmit_index].length =  m->len;
+  tx_ring[tx_transmit_index].cmd |= E1000_TXD_CMD_RS | E1000_TXD_CMD_EOP;
+  tx_mbufs[tx_transmit_index] = m;
+
+
+  regs[E1000_TDT] = (tx_transmit_index + 1)%TX_RING_SIZE;
+  release(&e1000_lock);
   return 0;
 }
 
@@ -111,10 +133,28 @@ e1000_recv(void)
 {
   //
   // Your code here.
-  //
+  //  
   // Check for packets that have arrived from the e1000
   // Create and deliver an mbuf for each packet (using net_rx()).
   //
+  while(1){
+    int rx_receive_index = (regs[E1000_RDT] + 1)%RX_RING_SIZE;
+    if((rx_ring[rx_receive_index].status & E1000_RXD_STAT_DD) == 0){
+      return;
+    }
+    rx_mbufs[rx_receive_index]->len = rx_ring[rx_receive_index].length;
+    net_rx(rx_mbufs[rx_receive_index]);
+
+    rx_mbufs[rx_receive_index] = mbufalloc(0);
+
+    if (!rx_mbufs[rx_receive_index])
+      panic("e1000");
+
+    rx_ring[rx_receive_index].addr = (uint64)(rx_mbufs[rx_receive_index]->head);
+    rx_ring[rx_receive_index].status = 0;
+    regs[E1000_RDT] = rx_receive_index;
+  }
+
 }
 
 void
